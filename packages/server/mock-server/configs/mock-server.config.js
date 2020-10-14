4e26af7c7932e59b6801c09d6fcc2304fedc51d9
yushicheng <1542874601@qq.com>
2020-10-14 12:56:38 +0800

    temp:临时节点标记 星期三 下午 12:56:37 十月 14日 2020 修改了 packages/server/mock-server/configs/mock-server.config.js 修改了 packages/server/mock-server/utils/getMockApi.js

--- packages/server/mock-server/configs/mock-server.config.js
+++ packages/server/mock-server/configs/mock-server.config.js
@@ -2,5 +2,6 @@
 // 配置参考 https://www.npmjs.com/package/mocker-api
 module.exports = {
   port: 5000,
+  ignore: "**/node_modules/**",
   changeHost: true
 }
--- packages/server/mock-server/utils/getMockApi.js
+++ packages/server/mock-server/utils/getMockApi.js
@@ -2,10 +2,13 @@ const glob = require("glob");
 const path = require("path");
 const { promisify } = require("es6-promisify");
 
+const getServerConfig = require("@/utils/getServerConfig");
+
 module.exports = async () => {
   try {
+    const { ignore } = await getServerConfig();
     const matchPattern = path.resolve(process.cwd(), "./**/**.mock.js");
-    const matchFiles = await promisify(glob)(matchPattern);
+    const matchFiles = await promisify(glob)(matchPattern, { ignore });
     const importTask = matchFiles.map((filePath) => {
       delete require.cache[require.resolve(filePath)];
       return require(filePath);
