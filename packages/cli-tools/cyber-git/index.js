1c35b1e82fce86e308200a5e8ccc61ef6ae05d91
yushicheng <1542874601@qq.com>
2020-10-15 16:49:04 +0800

    temp:临时节点标记 星期四 下午 4:49:03 十月 15日 2020 修改了 packages/cli-tools/cyber-git/index.js

--- packages/cli-tools/cyber-git/index.js
+++ packages/cli-tools/cyber-git/index.js
@@ -5,32 +5,8 @@ const json = require("@/package.json");
 
 const startProcess = require("@/scripts/startProcess");
 
-const isInit = require("@cyber-tools/git-assess-utils/isInit");
-const isChange = require("@cyber-tools/git-assess-utils/isChange");
-const hasPackageJson = require("@cyber-tools/git-assess-utils/hasPackageJson");
-const toast = require("@cyber-tools/cli-utils/toast");
-
-const confirmInitial = require("@/scripts/confirm/confirmInitial");
-const firstCommit = require("@/scripts/first-commit");
-
-
 (async () => {
-  if (!await hasPackageJson()) {
-    toast.warn("当前项目文件没有package.json文件");
-    process.exit(0);
-  };
-  if (!await isInit()) {
-    if (!await confirmInitial()) {
-      process.exit(0);
-    } else {
-      await firstCommit();
-      process.exit(0);
-    };
-  }
-  if (!await isChange()) {
-    toast.warn("当前项目文件没有变动");
-    process.exit(0);
-  };
+  await startProcess();
   program
     .usage("cyber-git <command>")
     .version(json.version);
