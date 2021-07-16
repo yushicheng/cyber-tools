40d4e2c6c0d8bcb2a5bae6c38a1bcd7844c2c290
yushicheng <1542874601@qq.com>
2021-07-16 21:52:38 +0800

    temp:临时节点标记 星期五 晚上 9:52:37 七月 16日 2021 修改了 packages/create/create-cyber-docs/configs/runtime.config.js

--- packages/create/create-cyber-docs/configs/runtime.config.js
+++ packages/create/create-cyber-docs/configs/runtime.config.js
@@ -1,5 +1,10 @@
 
 module.exports = [{
+  name:"docusaurus-docs-website",
+  value:{
+    remote: "https://github.com/cyber-scaffold/docusaurus-docs-website"
+  }
+},{
   name: "cyber-git-book",
   value: {
     remote: "https://github.com/cyber-scaffold/gitbook-project"
