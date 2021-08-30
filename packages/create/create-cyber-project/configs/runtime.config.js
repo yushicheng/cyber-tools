6a5862b47eb552bde89a0b65d496aa1ba536171f
yushicheng <1542874601@qq.com>
2021-08-30 19:04:30 +0800

    temp:临时节点标记 星期一 晚上 7:04:30 八月 30日 2021 修改了 packages/create/create-cyber-project/configs/runtime.config.js

--- packages/create/create-cyber-project/configs/runtime.config.js
+++ packages/create/create-cyber-project/configs/runtime.config.js
@@ -1,6 +1,11 @@
 
 
 module.exports = [{
+  name:"electron-umi-project",
+  value:{
+    remote: "https://github.com/cyber-scaffold/electron-umi-project"
+  }
+},{
   name: "umi-version3-project",
   value: {
     remote: "https://github.com/cyber-scaffold/umi-version3-project"
