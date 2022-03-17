27bcfa77712eebe8be10014f627b07c6f84deba4
yushicheng <1542874601@qq.com>
2022-03-17 19:26:11 +0800

    temp:临时节点标记 星期四 晚上 7:26:11 三月 17日 2022 修改了 packages/create/create-cyber-docs/package.json 修改了 packages/create/create-cyber-library/configs/runtime.config.js

--- packages/create/create-cyber-library/configs/runtime.config.js
+++ packages/create/create-cyber-library/configs/runtime.config.js
@@ -2,17 +2,11 @@
 module.exports = [{
   name: "es6-babel-library",
   value: {
-    devDependencies: {
-      "@cyber-tools/spa-build-tools": "*"
-    },
     remote: "https://github.com/cyber-scaffold/es6-babel-library"
   }
 }, {
   name: "es6-react-library",
   value: {
-    devDependencies: {
-      "@cyber-tools/spa-build-tools": "*"
-    },
     remote: "https://github.com/cyber-scaffold/es6-react-library"
   }
 }];
