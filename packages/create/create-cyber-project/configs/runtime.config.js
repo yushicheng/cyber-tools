5d06783a5bcc2eca77abf5e972b7cae0c1d19505
yushicheng <1542874601@qq.com>
2022-03-17 18:44:30 +0800

    temp:临时节点标记 星期四 晚上 6:44:30 三月 17日 2022 修改了 packages/create/create-cyber-project/configs/runtime.config.js

--- packages/create/create-cyber-project/configs/runtime.config.js
+++ packages/create/create-cyber-project/configs/runtime.config.js
@@ -10,13 +10,7 @@ module.exports = [{
   value: {
     remote: "https://github.com/cyber-scaffold/umi-version3-project"
   }
-}, {
-  name: "dva-app-project",
-  value: {
-    devDependencies: { "@cyber-tools/spa-build-tools": "*" },
-    remote: "https://github.com/cyber-scaffold/dva-spa-project"
-  }
-}, {
+},{
   name: "lerna-complex-project",
   value: {
     remote: "https://github.com/cyber-scaffold/lerna-complex-project"
