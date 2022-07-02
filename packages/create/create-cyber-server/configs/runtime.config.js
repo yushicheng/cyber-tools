8705de90a0439acc10268b77463252706185f7f9
yushicheng <1542874601@qq.com>
2022-07-02 11:44:24 +0800

    temp:临时节点标记 星期六 中午 11:44:24 七月 2日 2022 修改了 packages/create/create-cyber-server/configs/runtime.config.js

--- packages/create/create-cyber-server/configs/runtime.config.js
+++ packages/create/create-cyber-server/configs/runtime.config.js
@@ -5,9 +5,14 @@ module.exports = [{
   value: {
     remote: "https://github.com/cyber-scaffold/easy-koa-project"
   }
-},{
-  name:"easy-egg-microservice",
+}, {
+  name: "easy-egg-microservice",
   value: {
     remote: "https://github.com/cyber-scaffold/easy-egg-microservice"
+  }
+}, {
+  name: "easy-nest-microservice",
+  value: {
+    remote: "https://github.com/cyber-scaffold/easy-nest-microservice.git"
   }
 }];
