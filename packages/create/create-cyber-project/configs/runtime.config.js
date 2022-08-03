59a7efeac77d2368a65e1ad48a3b5c29f95ecd02
yushicheng <1542874601@qq.com>
2022-08-04 01:05:22 +0800

    temp:临时节点标记 星期四 凌晨 1:05:22 八月 4日 2022 修改了 packages/create/create-cyber-project/configs/runtime.config.js

--- packages/create/create-cyber-project/configs/runtime.config.js
+++ packages/create/create-cyber-project/configs/runtime.config.js
@@ -1,16 +1,21 @@
 
 
 module.exports = [{
-  name:"electron-umi-project",
-  value:{
+  name: "electron-chimera-project",
+  value: {
+    remote: "https://github.com/cyber-scaffold/electron-chimera-project"
+  }
+}, {
+  name: "electron-umi-project",
+  value: {
     remote: "https://github.com/cyber-scaffold/electron-umi-project"
   }
-},{
+}, {
   name: "umi-version3-project",
   value: {
     remote: "https://github.com/cyber-scaffold/umi-version3-project"
   }
-},{
+}, {
   name: "lerna-complex-project",
   value: {
     remote: "https://github.com/cyber-scaffold/lerna-complex-project"
