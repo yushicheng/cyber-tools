0a7fec441f5103b625da3c31f7b66119a5dbbcf7
yushicheng <1542874601@qq.com>
2023-05-14 23:20:23 +0800

    temp:临时节点标记 星期日 晚上 11:20:23 五月 14日 2023 修改了 packages/create/create-cyber-tools/index.js 修改了 packages/create/create-cyber-tools/package.json

--- packages/create/create-cyber-tools/index.js
+++ packages/create/create-cyber-tools/index.js
@@ -1,8 +1,18 @@
 require("@cyber-tools/cli-utils/initial");
 require("module-alias").addAlias("@", __dirname);
+const { bootstrap } = require("global-agent");
 const createTools = require("@/scripts/create-tools");
 const selectTemplate = require("@/utils/selectTemplate");
 
+if (process.env.http_proxy) {
+  console.log(process.env.http_proxy);
+  process.env.GLOBAL_AGENT_HTTP_PROXY = process.env.http_proxy
+};
+if (process.env.https_proxy) {
+  console.log(process.env.https_proxy);
+  process.env.https_proxy && (process.env.GLOBAL_AGENT_HTTPS_PROXY = process.env.https_proxy);
+};
+bootstrap();
 
 (async () => {
   try {
