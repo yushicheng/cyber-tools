7eae6431ca36cc43d74f8813edb2e5e44c360f63
frankleo <154284601@qq.com>
2023-05-19 15:21:29 +0800

    temp:临时节点标记 星期五 下午 3:21:29 五月 19日 2023 修改了 packages/create/create-cyber-tools/configs/runtime.config.js

--- packages/create/create-cyber-tools/configs/runtime.config.js
+++ packages/create/create-cyber-tools/configs/runtime.config.js
@@ -3,9 +3,14 @@ module.exports = [{
   name: "single-commander-cli",
   value: {
     remote: "https://github.com/cyber-scaffold/single-commander-cli"
-  }  
-},{
-  name: "multiple-commander-cli",
+  }
+}, {
+  name: "multiple-commander-cli-ts",
+  value: {
+    remote: "https://github.com/cyber-scaffold/typescript-command"
+  }
+}, {
+  name: "multiple-commander-cli-js",
   value: {
     remote: "https://github.com/cyber-scaffold/multiple-commander-cli"
   }
