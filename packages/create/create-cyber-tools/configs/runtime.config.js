9ec1736bdea639983bcde3d69baf7e15da83941b
frank.leo <1542874601@qq.com>
2024-06-12 16:55:10 +0800

    temp:临时节点标记 星期三 下午 4:55:10 六月 12日 2024 修改了 packages/create/create-cyber-tools/configs/runtime.config.js

--- packages/create/create-cyber-tools/configs/runtime.config.js
+++ packages/create/create-cyber-tools/configs/runtime.config.js
@@ -1,17 +1,21 @@
 
-module.exports = [{
-  name: "single-commander-cli",
-  value: {
-    remote: "https://github.com/cyber-scaffold/single-commander-cli"
-  }
-}, {
-  name: "multiple-commander-cli-ts",
-  value: {
-    remote: "https://github.com/cyber-scaffold/typescript-command"
-  }
-}, {
-  name: "multiple-commander-cli-js",
-  value: {
-    remote: "https://github.com/cyber-scaffold/multiple-commander-cli"
-  }
-}];
+module.exports = [
+  // {
+  //   name: "single-commander-cli",
+  //   value: {
+  //     remote: "https://github.com/cyber-scaffold/single-commander-cli"
+  //   }
+  // },
+  {
+    name: "multiple-commander-cli-ts",
+    value: {
+      remote: "https://github.com/cyber-scaffold/typescript-command"
+    }
+  },
+  // {
+  //   name: "multiple-commander-cli-js",
+  //   value: {
+  //     remote: "https://github.com/cyber-scaffold/multiple-commander-cli"
+  //   }
+  // }
+];
